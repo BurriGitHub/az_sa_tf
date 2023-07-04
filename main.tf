f5749956c78a8891ef28f5eda439c02a76cfec86
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2023-07-04 19:54:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West Europe"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+# Output the storage account connection string
+output "connection_string" {
+  value = azurerm_storage_account.example.primary_connection_string
+}
