482c9e00a5bc5e46c4596012785f59effbf0402e
BurriGitHub <135586680+BurriGitHub@users.noreply.github.com>
2023-09-06 15:35:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "example" {
 
 # Create a storage account
 resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount"
+  name                     = "01mystorageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
